a32042888da2377c6b10d695cfd7567fff31785f
hseiji <henrique.hst@gmail.com>
2022-02-19 15:57:19 -0500

    set up interface to handle user input (setupInput) and handle user input

--- play.js
+++ play.js
@@ -2,3 +2,24 @@ const { connect } = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  // event listener
+  stdin.on('data', (key) => handleUserInput(key));
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // \u0003 maps to ctrl+c input. Terminate the terminal
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+setupInput();
