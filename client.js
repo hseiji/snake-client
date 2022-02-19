1a364f4c7f916bd3fb71cb27a460445aa10c3d9c
hseiji <henrique.hst@gmail.com>
2022-02-19 11:05:07 -0500

    created client'js file and exported function using es6 shorthand syntax

---
+++ client.js
@@ -0,0 +1,24 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here,
+    port: 50541 // PORT number here. It should be the same as the constants.js file
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  // event handler to handle incoming data
+  conn.on('data', () => {
+    console.log("you ded cuz you idled");
+  });
+
+  return conn;
+};
+
+// Exporting function using ES6 Shorthand syntax
+module.exports = {
+  connect
+};
